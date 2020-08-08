347b4d37eeaeeaa5a466d07edbd78bc8bb30e167
xuexin <xuexin@cloudwalk.cn>
2020-08-08 20:47:12 +0800

    chore: 修改loading样式

--- src/base/loading/index.vue
+++ src/base/loading/index.vue
@@ -2,12 +2,25 @@
   <transition name="custom-loading-fade">
     <!--loading蒙版-->
     <div
+      style="position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      background: rgba(0,0,0,0.3 );
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #fff;"
       v-show="visible"
       class="custom-loading-mask"
     >
       <!--loading中间的图标-->
       <div class="custom-loading-spinner">
-        <i class="custom-spinner-icon"></i>
+        <img
+          src="@/assets/spinner.gif"
+          alt=""
+        >
         <!--loading上面显示的文字-->
         <p class="custom-loading-text">{{ text }}</p>
       </div>
