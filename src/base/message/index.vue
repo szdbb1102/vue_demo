8a1315b0c2fdbe6d8d14b38c4bbe0b41530b3516
xuexin <xuexin@cloudwalk.cn>
2020-08-12 20:29:08 +0800

    chore: 新增文件

---
+++ src/base/message/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'xxMessage',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+</style>
